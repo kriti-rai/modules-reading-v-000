01296e901f3a7677a0a7e5aea1dc55eb412e7961
Kriti Rai <kriti.rai@gmail.com>
2018-02-14 20:57:42 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -9,13 +9,13 @@ module FancyDance
       "I'm twirling!"
     end
 
-    def jump 
+    def jump
       "Look how high I'm jumping!"
     end
 
     def pirouette
       "I'm doing a pirouette"
-    end 
+    end
 
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
